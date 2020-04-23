de70f62c2bb74e8b8ea43ffdcb6368eecc5080c3
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-04-23 23:15:19 +0000

    chore: display component

---
+++ src/components/layouts/Display.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Tracks from './../tracks/Tracks';
+
+const Display = () => {
+  return (
+    <div>
+      <Tracks />
+    </div>
+  );
+};
+
+export default Display;
