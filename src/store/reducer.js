92bcd1107753c219f487a226a094d2642453e6a4
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-04-23 23:19:26 +0000

    chore: store data from api

---
+++ src/store/reducer.js
@@ -0,0 +1,37 @@
+import {
+  FETCH_TRACKS_REQUEST,
+  FETCH_TRACKS_SUCCESS,
+  FETCH_TRACKS_FAILURE,
+} from './trackTypes';
+
+const initState = {
+  trackList: [],
+  loading: false,
+  error: '',
+  heading: 'Top 10 Tracks',
+};
+
+const reducer = (state = initState, action) => {
+  switch (action.type) {
+    case FETCH_TRACKS_REQUEST:
+      return { ...state, loading: true };
+    case FETCH_TRACKS_SUCCESS:
+      return {
+        ...state,
+        loading: false,
+        trackList: action.payload,
+        error: '',
+      };
+    case FETCH_TRACKS_FAILURE:
+      return {
+        ...state,
+        loading: true,
+        trackList: [],
+        error: action.payload,
+      };
+    default:
+      return state;
+  }
+};
+
+export default reducer;
