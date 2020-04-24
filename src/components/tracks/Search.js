8c46cdb0239008498c07c4563c55b13ad1347713
Huzaifah Attah <hhuzaifah050@gmail.com>
2020-04-24 13:57:47 +0000

    chore: fetch data and sent to store

--- src/components/tracks/Search.js
+++ src/components/tracks/Search.js
@@ -1,42 +1,65 @@
-// import React, { Component } from 'react';
-// import { connect } from 'react-redux';
-// import { search } from '../../store/actions';
+import React, { Component } from 'react';
+import { connect } from 'react-redux';
+import { searchTrack } from '../../store/actions';
+import axios from 'axios';
 
-// class Search extends Component {
-//   state = {
-//     trackList: [{ track: 'search', id: '1' }],
-//     heading: 'searched',
-//   };
+class Search extends Component {
+  state = {
+    trackTitle: '',
+  };
 
-//   handleChange = (e) => {
-//     this.setState({
-//       [e.target.name]: e.target.value,
-//     });
-//   };
+  handleChange = (e) => {
+    this.setState({
+      [e.target.name]: e.target.value,
+    });
+  };
 
-//   handleSubmit = (e) => {
-//     e.preventDefault();
-//     this.props.search(this.state);
-//   };
-//   render() {
-//     return (
-//       <div>
-//         <form onSubmit={this.handleSubmit}>
-//           <input
-//             type="text"
-//             name="trackList"
-//             id="trackList"
-//             onChange={this.handleChange}
-//           />
-//           <button type="submit">Search</button>
-//         </form>
-//       </div>
-//     );
-//   }
-// }
+  handleSubmit = (e) => {
+    e.preventDefault();
+    if (this.state.trackTitle) {
+      axios
+        .get(
+          `http://cors-anywhere.herokuapp.com/https://api.musixmatch.com/ws/1.1/track.search?q_track=${this.state.trackTitle}&page_size=10&page=1&s_track_rating=desc&apikey=${process.env.REACT_APP_MM_KEY}`
+        )
+        .then((res) => {
+          console.log(res.data.message.body.track_list);
+          this.props.searchTrack(res.data.message.body.track_list);
+        })
+        .catch((err) => console.log(err.message));
+    } else {
+      return;
+    }
+  };
+  render() {
+    return (
+      <div className="card card-body mb-4 p-4">
+        <h1 className="display-4 text-center">
+          <i className="fas fa-music"></i> Search For a Song
+        </h1>
+        <p className="lead text-center">Get the Lyrics for any Song</p>
+        <form className="form-group" onSubmit={this.handleSubmit}>
+          <input
+            type="text"
+            name="trackTitle"
+            id="trackList"
+            onChange={this.handleChange}
+            className="form-control form-control-lg"
+            placeholder="Song title.."
+          />
+          <button
+            className="btn btn-primary btn-lg btn-block mt-3"
+            type="submit"
+          >
+            Get Lyrics
+          </button>
+        </form>
+      </div>
+    );
+  }
+}
 
-// const mapDispatchToProps = {
-//   search: search,
-// };
+const mapDispatchToProps = {
+  searchTrack: searchTrack,
+};
 
-// export default connect(null, mapDispatchToProps)(Search);
+export default connect(null, mapDispatchToProps)(Search);
